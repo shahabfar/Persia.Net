c15288dad9d1d7f3c3e792a113a0bcb993cbf871
Majid Shahabfar <Majid@SHAHABFAR>
2024-03-08 22:31:55 +0330

    Add project files.

---
+++ src/Persia.Net/DateTimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Persia.Net;
+
+internal static class DateTimeExtensions
+{
+    public static PersianDateTime ToPersianDateTime(this DateTime? date)
+    {
+        if (!date.HasValue)
+            throw new ArgumentNullException(nameof(date));
+
+        return Converter.ConvertToPersian(date.Value);
+    }
+}
